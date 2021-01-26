cd06368faefe36a3e6d9a475a15ef41afbdd3a5c
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-01-26 13:27:01 +0100

    Fixed level 1 pickaxe not finding anything

--- commands/mine.js
+++ commands/mine.js
@@ -88,7 +88,7 @@ module.exports = {
       const msg = await message.channel.send(embed);
       await client.setTimeout(() => {
         const emb = msg.embeds[0];
-				if(ore.level <= client.miner.get(key, 'pickaxe')){
+				if(ore.level < client.miner.get(key, 'pickaxe')){
         	emb.setTitle(`You found ${ore.name.toLowerCase()} in the mines`)
         	.setDescription(`You require a pickaxe of level ${ore.level} to mine this ore, your current pickaxe level is ${client.miner.get(key, 'pickaxe')}.`)
         	.setColor('BLUE')
