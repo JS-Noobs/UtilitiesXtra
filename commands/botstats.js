0199b33a2a013c83f5c73a8e44639941c65f2f35
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-01-27 09:36:54 +0100

    Rounded the memory

--- commands/botstats.js
+++ commands/botstats.js
@@ -28,16 +28,16 @@ module.exports = {
     		cpuUsage = Math.round(100 * cpu.times[type] / total);
     	};
 		};
-    const totalMem = os.totalmem() / 1000000;
-    const freeMem = os.freemem() / 1000000;
+    const totalMem = os.totalmem();
+    const freeMem = os.freemem();
     const percent = ((totalMem/freeMem) / 100).toFixed(2);
     const embed = new MessageEmbed()
     .setTitle('Bot statistics')
     .addField('CPU Usage', `${cpuUsage}%`, true)
     .addField('\u200b', '\u200b', true)
     .addField('\u200b', '\u200b', true)
-    .addField('Total Memory', totalMem+'MB', true)
-    .addField('Free Memory', freeMem+'MB', true)
+    .addField('Total Memory', Math.round(totalMem/1000000)+'MB', true)
+    .addField('Free Memory', Math.round(freeMem/1000000)+'MB', true)
     .addField('Memory Used', percent, true)
     
     message.channel.send(embed)
