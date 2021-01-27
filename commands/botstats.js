e24590cbc027a82a5d18aa0ccbdf92e2ab513c2c
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-01-27 09:32:13 +0100

    Updated cpuUsage

--- commands/botstats.js
+++ commands/botstats.js
@@ -14,7 +14,20 @@ module.exports = {
   development: false,
   ea: false,
 	execute: async(message, args, client) => {
-    const cpuUsage = os.cpus();
+    const cpus = os.cpus();
+		let cpuUsage
+		for(var i = 0, len = cpus.length; i < len; i++) {
+    	console.log("CPU %s:", i);
+    	var cpu = cpus[i], total = 0;
+
+    	for(var type in cpu.times) {
+    		total += cpu.times[type];
+    	};
+
+    	for(type in cpu.times) {
+    		cpuUsage = Math.round(100 * cpu.times[type] / total);
+    	};
+		};
     const totalMem = os.totalmem();
     const freeMem = os.freemem();
     const percent = ((totalMem/freeMem) / 100).toFixed(2);
