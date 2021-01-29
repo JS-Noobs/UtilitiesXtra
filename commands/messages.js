d9080017f38ed482b8b330cf92a65d5870b18cd8
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-01-29 18:27:27 +0100

    Update messages.js

--- commands/messages.js
+++ commands/messages.js
@@ -3,6 +3,7 @@ const shop = require('../shop.json');
 const upgrades = require('../upgrades.json');
 const monsters = require('../monsters.json');
 const timeout = new Set();
+const ms = require('pretty-ms');
 module.exports = {
 	name: 'messages',
   alias: ['msgs', 'messagecount', 'msgcount'],
@@ -14,8 +15,13 @@ module.exports = {
   ea: false,
 	execute: async(message, args, client) => {
     const key = `${message.guild.id}-${message.member.id}`;
+		const curDate = new Date()
+		const date = new Date()
+		const nextDay = new Date(date.setDate(date.getDate()+1));
+		const msLeft = ms(nextDat-curDate)
     const embed = new MessageEmbed()
-    .setTitle(`You have ${client.messages.get(key, 'total')} messages sent`)
+    .setDescription(`Total messages: ${client.messages.get(key, 'total')}\nDaily messages: ${client.messages.get(key, 'dailyMessages')}`)
+		.setFooter(`${msLeft} until daily reset`)
     .setColor(`BLUE`)
     return message.channel.send(embed);
   },
