80a2b97f3c2e710038b315f492bd851cac859c5f
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-02-03 09:00:02 +0100

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,22 @@
+const {MessageEmbed} = require('discord.js');
+const shop = require('../shop.json');
+const upgrades = require('../upgrades.json');
+const monsters = require('../monsters.json');
+const timeout = new Set();
+const ytdl = require('ytdl-core');
+module.exports = {
+	name: 'play',
+  alias: ['youtube', 'yt'],
+	description: 'Play music',
+  category: 'music',
+  permissions: [],
+  botpermissions: ['CONNECT', 'SPEAK'],
+  development: false,
+  ea: false,
+	execute: async(message, args, client) => {
+    const vc = message.member.voice.channel;
+    if(!vc) return message.channel.send(`You have to be in a voice channel to play music`);
+    const connection = await vc.join();
+    const dispatcher = connection.play(ytdl(args[0]));
+  },
+};
