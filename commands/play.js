6a2b521300ebc611c6bb7ccfeceb82ffab2369ff
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-02-06 23:28:56 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -3,7 +3,7 @@ const shop = require('../shop.json');
 const upgrades = require('../upgrades.json');
 const monsters = require('../monsters.json');
 const timeout = new Set();
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
 module.exports = {
 	name: 'play',
   alias: ['youtube', 'yt'],
@@ -17,7 +17,7 @@ module.exports = {
     const vc = message.member.voice.channel;
     if(!vc) return message.channel.send(`You have to be in a voice channel to play music`);
     const connection = await vc.join();
-    const dispatcher = await connection.play(ytdl(args[0], { quality: 'highestaudio' }))
+    const dispatcher = await connection.play(await ytdl(args[0]), { type: 'opus' });
     dispatcher.on('debug', debug => {
 			console.log(debug);
 		});
