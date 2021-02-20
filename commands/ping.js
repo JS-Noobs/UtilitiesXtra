f64b3d7213ff0c456b837da6ceaa77501174b23b
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2021-02-20 08:26:20 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -11,11 +11,11 @@ module.exports = {
   ea: false,
 	execute: async(message, args, client) => {
      const msg = await message.channel.send(`Pinging...`);
-     const ping = await ms(Date.now() - msg.createdAt);
+     const ping = await Date.now() - msg.createdAt;
      msg.delete()
      const embed = new MessageEmbed()
      .setTitle(`Ping`)
-     .setDescription(`:ping_pong: ${ping}\n:heartbeat: ${client.ws.ping}ms`)
+     .setDescription(`:ping_pong: ${ping}ms or ${ping/1000}s\n:heartbeat: ${client.ws.ping}ms or ${client.ws.ping/1000}s`)
      .setColor('GREEN')
      message.channel.send(embed);
   },
