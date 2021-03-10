25bc0b6528f542e3b9b6be75e5e79c7062415d95
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-10 22:32:18 +0000

    fix #2

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,5 +1,8 @@
 module.exports = async (client, reaction, user) => {
+  if(reaction.partial) await reaction.fetch()
   if(client.votechannels.get(reaction.message.guild.id, 'channels').includes(reaction.message.channel.id)) {
-    if(user.id === reaction.message.author.id) reaction.users.remove(user.id)
+    if(user.id === reaction.message.author.id) {
+      reaction.users.remove(user.id);
+    };
   };
 };
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
 });
 app.use('/', router);
 //==========================================================//
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['REACTION']});
 //==========================================================//
 client.events = new Discord.Collection();
 client.commands = new Discord.Collection();
