e1d3795d78a7fbcd65a5cad6cfe34ccc871b64ad
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-10 23:51:17 +0000

    Fix #1

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,7 +3,7 @@ module.exports = async (client, reaction, user) => {
   if(user.bot) return;
   if(client.votechannels.get(reaction.message.guild.id, 'channels').includes(reaction.message.channel.id)) {
     const embed = reaction.message.embeds[0]
-    if(embed.footer.includes(user.id)) {
+    if(embed.footer.text.includes(user.id)) {
       reaction.users.remove(user.id);
     };
   };
