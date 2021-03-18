f72a1a7a19e369029cec7cbde02eea7bb33add93
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-18 12:10:19 +0000

    Partnership test #05

--- commands/partner.js
+++ commands/partner.js
@@ -12,7 +12,7 @@ module.exports = {
   execute: async (message, args, client) => {
     if(!args[0]) return message.channel.send(new MessageEmbed().setTitle('Please use one of following options.').addField('Apply', 'Apply for partnership with the bot', true).addField('\u200b', '\u200b', true).addField('Info', 'Check current information about your pending application', true).addField('Requirements', 'Show requirements to apply for partnership', true).addField('\u200b', '\u200b', true).addField('Perks', 'View the perks that comes with a partnership', true))
     if(args[0] === `requirements`) {
-      const members = 100, botAge = 7 * 24 * 60 * 60 * 1000;
+      const members = 100, botAge = Date.parse(message.guild.joinedAt) + 7 * 24 * 60 * 60 * 1000;
       const membersNeed = message.guild.memberCount >= members ? '✅' : members - message.guild.memberCount + ' more members needed';
       const botJoined = Date.parse(message.guild.joinedAt);
       const daysNeeded = botJoined >= botAge ? '✅' : ms(botAge - botJoined)
