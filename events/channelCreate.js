c15f9b2e8ac2b386fba4da6b4f2c8b29afa82295
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-19 23:00:23 +0000

    Fixed some stuff for channelCreate event (yes syntax)

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 module.exports = async (client, channel) => {
   if(channel.type === 'dm') return;
-  if (!message.guild.roles.cache.has(client.botsettings.get(message.guild.id, 'mutedRole'))) await message.guild.roles.create({
+  if (!channel.guild.roles.cache.has(client.botsettings.get(channel.guild.id, 'mutedRole'))) await channel.guild.roles.create({
       data: {
         name: 'Muted',
         color: 'GRAY',
@@ -10,13 +10,13 @@ module.exports = async (client, channel) => {
         ]
       },
     }).then(role => {
-      client.botsettings.set(message.guild.id, role.id, 'mutedRole')
-      message.guild.channels.cache.forEach(x => x.updateOverwrite(role.id, {
+      client.botsettings.set(channel.guild.id, role.id, 'mutedRole')
+      channel.guild.channels.cache.forEach(x => x.updateOverwrite(role.id, {
         SEND_MESSAGES: false
       }));
     });
 
-    const muted = message.guild.roles.cache.get(client.botsettings.get(message.guild.id, 'mutedRole')) || message.guild.roles.cache.find(x => x.name.toLowerCase().includes('muted'));
+    const muted = channel.guild.roles.cache.get(client.botsettings.get(channel.guild.id, 'mutedRole')) || channel.guild.roles.cache.find(x => x.name.toLowerCase().includes('muted'));
     channel.updateOverwrite(muted.id, {
       SEND_MESSAGES: false
     });
