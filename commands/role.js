6406d92cd3370e794dfe9bc48314d64c9b245a12
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-19 22:55:53 +0000

    !role <info> added

--- commands/role.js
+++ commands/role.js
@@ -11,6 +11,22 @@ module.exports = {
   botpermissions: [],
   developer: false,
   execute(message, args, client) {
+    if(!args[0]) return message.channel.send(`Please mention a role or use its id.`);
+    const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
+    if(!role) return message.channel.send(`Invalid role.`);
+    else if(!args[1]) return message.channel.send(`Please use one of following options \`info\``);
+    if(args[1] === 'info'){
+      let above = message.guild.roles.cache.find(x => x.position === role.position+1) || `No role is higher than ${role.name}.`
+      let below = message.guild.roles.cache.find(x => x.position === role.position-1) || `No role is lower than ${role.name}.`
+      const embed = new MessageEmbed()
+      .setTitle(role.name)
+      .setColor(role.color)
+      .addField(`Role above`, `${above.name} (${above.position})`, true)
+      .addField(`Current`, `${role.name} (${role.position})`, true)
+      .addField(`Role below`, `${below.name} (${below.position})`, true)
+      .setDescription(`${role.permissions.has('ADMINISTRATOR') ? 'This role has admin permissions' : 'This role does not have admin permissions'}\nThere is ${role.members.cache.size} members with this role ${role.members.cache.has(message.author.id) ? 'You are one of them' : 'You are not one of them'}`)
 
+      return message.channel.send(embed);
+    }
   },
 };
