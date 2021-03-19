0bad5cdbc9d4d444ac4ddbbee74d3e998a1121ba
LightBlueGamer <timjonsson03@hotmail.com>
2021-03-19 19:06:05 +0000

    Fixed more syntax

--- events/permissions.js
+++ events/permissions.js
@@ -2,7 +2,8 @@ module.exports = async (client, message) => {
   client.permissions.ensure(message.guild.id, {});
   client.commands.forEach(cmd => {
     if(cmd.developer === false) {
-      client.permissions.set(message.guild.id, {cmd.name: []});
+      const name = cmd.name;
+      client.permissions.set(message.guild.id, {name: []});
     };
   });
 };
